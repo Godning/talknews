3aead73fb2fc29fcfc9dc2c5329666638397583e
Godning <736397792@qq.com>
2016-11-29 18:06:12 +0800

    Update clawer.py

--- clawer.py
+++ clawer.py
@@ -7,15 +7,7 @@ Created on 2016-11-23
 import pymysql.cursors
 from datetime import date
 
-config = {
-    'host': '127.0.0.1',
-    'port': 3306,
-    'user': 'root',
-    'password': '123',
-    'db': 'clawer',
-    'charset': 'utf8mb4',
-    'cursorclass': pymysql.cursors.DictCursor,
-}
+from config import config
 import urllib2
 from bs4 import BeautifulSoup
 import socket
@@ -133,20 +125,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-'''
-try:
-    with connection.cursor() as cursor:
-        # 执行sql语句，插入记录
-        sql = 'select * from news'
-        cursor.execute(sql,)
-    # 没有设置默认自动提交，需要主动提交，以保存所执行的语句
-    result = cursor.fetchone()
-    #print(result)
-    #for key in result:
-    #    print result[key]
-    print result['title']
-    connection.commit()
-
-finally:
-    connection.close()
-'''
